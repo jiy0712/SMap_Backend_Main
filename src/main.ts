5ba4a53656b17cdcf70be77319e3e2307f069237
jiy0712 <s2325@e-mirim.hs.kr>
2025-10-10 21:33:31 +0900

    fit : CommonJS 호환 방식으로 변경

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-import cors from 'cors';
+import * as cors from 'cors'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,16 +12,17 @@ async function bootstrap() {
   app.use(
     cors({
       origin: [
-        'http://localhost:3000', // 로컬용 프론트
-        'http://98.88.33.105',   // 배포용 프론트
+        'http://localhost:3000', // 로컬 프론트
+        'http://98.88.33.105',   // 배포 프론트
       ],
-      credentials: true, 
+      credentials: true,
     }),
   );
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(3000);
-  console.log(`Server running on http://localhost:3000`);
+  console.log(`http://localhost:3000`);
 }
+
 bootstrap();
