72d48d02611e77cc2197949e6114f6d72da3c62e
박지영 <s2325@e-mirim.hs.kr>
2025-10-10 14:42:47 +0900

    fit : cors 변경 (테스트 중)

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-  app.enableCors({
-    origin: 'http://98.88.33.105',
-    credentials: true,
-  });
+
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:3000', // 로컬용 프론트
+        'http://98.88.33.105',   // 배포용 프론트
+      ],
+      credentials: true, 
+    }),
+  );
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(3000);
+  console.log(`Server running on http://localhost:3000`);
 }
 bootstrap();
