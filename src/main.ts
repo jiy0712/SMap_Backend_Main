34ac0338d91c3d04730bf71378790e11c5ee69ef
jiy0712 <s2325@e-mirim.hs.kr>
2025-11-13 21:29:48 +0900

    feat : cors 추가 (프론트 URL)

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,9 @@ async function bootstrap() {
         'http://localhost:3000', // 로컬 프론트
         'http://98.88.33.105',   // 배포 프론트
         'https://port-0-smap-backend-main-mhkpzrkrde061e33.sel3.cloudtype.app',
+        'https://s-map-front.vercel.app',
+        'https://s-map-front-git-main-lees-projects-24c32651.vercel.app',
+        'https://s-map-front-1ani35zs5-lees-projects-24c32651.vercel.app',
       ],
       credentials: true,
     }),
